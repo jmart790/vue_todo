48859b5b6faa0880334c2b997cb430ca0c0495bd
Josh <jmart790@fiu.edu>
2019-10-12 20:22:42 -0400

    added the filter component to show active, complete, or all tasks

---
+++ src/components/FilterTodos.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <button :class="{active: filter == 'all'}" @click="filterTodos('all')">All</button>
+    <button :class="{active: filter == 'active'}" @click="filterTodos('active') ">Active</button>
+    <button :class="{active: filter == 'completed'}" @click="filterTodos('completed') ">Completed</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FilterTodos",
+  props: ["filter"],
+  methods: {
+    filterTodos(filter) {
+      this.$emit("filter-todos", filter);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.active {
+  background-color: aqua;
+}
+</style>
