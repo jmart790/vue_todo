1cba49a3c517fee669def0f4c9b59ea88e671934
Josh <jmart790@fiu.edu>
2019-10-12 20:21:55 -0400

    added a counter component to show the remaining incompleted tasks total

---
+++ src/components/Counter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="todo-count">{{remaining}} left!</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Counter",
+  props: ["remaining"]
+};
+</script>
+
+<style scoped>
+.todo-count {
+  text-align: center;
+  font-size: 1rem;
+  font-weight: bold;
+  padding: 0.5rem;
+}
+</style>
