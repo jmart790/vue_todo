9858e92f3f0e646b22e2e0addd6cbc147cf23aa7
Josh <jmart790@fiu.edu>
2019-10-12 20:21:03 -0400

    added a component to remove all completed task from list permanently

---
+++ src/components/ClearCompleted.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <button v-if="showClearCompleteBtn" @click="clearCompleted">Clear Completed</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ClearCompleted",
+  props: ["todos"],
+  methods: {
+    clearCompleted() {
+      this.$emit("clear-completed");
+    }
+  },
+  computed: {
+    showClearCompleteBtn() {
+      return this.todos.filter(todo => todo.completed).length > 0;
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
