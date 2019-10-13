963c4bbfc59308f332ff1c30ed13ad0b36407c1d
Josh <jmart790@fiu.edu>
2019-10-12 20:20:11 -0400

    added a component to check all the task as completed

---
+++ src/components/CheckAll.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input type="checkBox" :checked="!anyRemaining" @change="checkAllTodos" />
+    <span>&nbsp;Check all</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CheckAll",
+  props: ["anyRemaining"],
+  methods: {
+    checkAllTodos() {
+      this.$emit("check-all");
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
