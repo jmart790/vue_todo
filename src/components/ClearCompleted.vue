8788b6fdc90a232bd89e0a154c31bb89eae3cfe3
Josh <jmart790@fiu.edu>
2019-10-16 13:11:03 -0400

    added style for clear completion button

--- src/components/ClearCompleted.vue
+++ src/components/ClearCompleted.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    <button v-if="showClearCompleteBtn" @click="clearCompleted">Clear Completed</button>
+  <div class="clear-completed-container" v-if="showClearCompleteBtn">
+    <button name="clearCompleted" @click="clearCompleted">
+      <i class="fas fa-minus"></i>
+    </button>
+    <label for="clearCompleted">Clear Completed</label>
   </div>
 </template>
 
@@ -21,5 +24,28 @@ export default {
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.clear-completed-container {
+  display: grid;
+  align-items: center;
+  grid-template-columns: auto 1fr;
+
+  button {
+    box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.187);
+    outline: none;
+    border: none;
+    border-radius: 50%;
+    color: white;
+    font-size: 1rem;
+    cursor: pointer;
+    width: 2rem;
+    height: 2rem;
+    margin: 0.5rem;
+    background-image: linear-gradient(to left, #f43737 0%, #ff9c7e 100%);
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.084);
+  }
+  button:hover {
+    box-shadow: 2px 2px 4px rgba(255, 0, 0, 0.187);
+  }
+}
 </style>
